32367e9a3499989e1d51b92cfb7e62e97079f514
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-13 23:17:39 +0200

    Update Screenshot.ps1

--- Screenshot.ps1
+++ Screenshot.ps1
@@ -7,3 +7,6 @@ $bitmap = New-Object System.Drawing.Bitmap $bounds.Width, $bounds.Height
 $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
 $graphics.CopyFromScreen([System.Drawing.Point]::Empty, [System.Drawing.Point]::Empty, $bounds.Size)
 $bitmap.Save($screenshotPath, [System.Drawing.Imaging.ImageFormat]::Png)
+
+# Open the screenshot file automatically
+Start-Process $screenshotPath
