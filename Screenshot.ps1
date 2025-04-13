cd812e793940a3c1e9e5bb9eb0266099e1961276
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-13 23:16:32 +0200

    Create Screenshot.ps1

---
+++ Screenshot.ps1
@@ -0,0 +1,9 @@
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$screenshotPath = "$scriptPath\screenshot.png"
+
+Add-Type -AssemblyName System.Windows.Forms
+$bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+$bitmap = New-Object System.Drawing.Bitmap $bounds.Width, $bounds.Height
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphics.CopyFromScreen([System.Drawing.Point]::Empty, [System.Drawing.Point]::Empty, $bounds.Size)
+$bitmap.Save($screenshotPath, [System.Drawing.Imaging.ImageFormat]::Png)
