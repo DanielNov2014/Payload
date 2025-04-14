0c7bc5c5217b4d53bbbcfc10f3d203e112f6a50e
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-14 14:46:12 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,7 @@
-start talk2.vbs
+echo Dim speech >> talk.vbs
+echo Set speech = CreateObject("SAPI.SpVoice") >> talk.vbs
+echo speech.Speak "Hello, again user this will run every time you start your computer" >> talk.vbs
+
+start talk.vbs
+timeout 3 > nul
+del /f /q "talk.vbs"
